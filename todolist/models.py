7be1e0afa16720c05816f88eb4314037a29766bd
mr_eugene <roberttimethone@gmail.com>
2025-11-18 19:40:52 +0100

    created models

--- todolist/models.py
+++ todolist/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 from django.utils import timezone
 
+
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
