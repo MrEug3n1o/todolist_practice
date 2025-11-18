3e6b1dc0bdca73d39952c1af9da3e333b6533e70
mr_eugene <roberttimethone@gmail.com>
2025-11-18 19:48:31 +0100

    created views and html basics

--- todolist/views.py
+++ todolist/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView
+from .models import Task, Tag
 
-# Create your views here.
+def home(request):
+    tasks = Task.objects.all()
+    return render(request, 'todolist/home.html', {'tasks': tasks})
+
+def toggle_task_status(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.toggle_status()
+    return redirect('home')
+
+
+class TagListView(ListView):
+    model = Tag
+    template_name = 'todolist/tag_list.html'
+    context_object_name = 'tags'
